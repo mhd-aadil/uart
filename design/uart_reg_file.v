fcae08c8936afc0654625301178101221bd99537
Mohammed Aadil <adhilmohamed7@gmail.com>
2026-06-02 08:21:33 +0000

    Updated UART register file testbench

--- design/uart_reg_file.v
+++ design/uart_reg_file.v
@@ -1,3 +1,12 @@
+/*Bit	Name	Purpose
+[1:0]	word length	5/6/7/8 bits
+[2]	stop bits	1 or 2 stop bits
+[3]	parity enable	enable parity
+[4]	parity type	even/odd
+[5]	stick parity	advanced
+[6]	break control	force TX low
+[7]	DLAB	divisor latch access
+*/
 module uart_reg_file(
 input clk,
 input rstn,
@@ -16,3 +25,17 @@ input framing_error,
 input parity_error,
 
 output reg [7:0] rdata,
+output reg  [7:0] ier,
+output reg  [7:0] lcr,
+output reg  [15:0] divisor,
+
+output reg [7:0] tx_fifo_data_in,
+output reg tx_fifo_wr
+output reg rx_fifo_rd
+);
+
+wire dlab;
+assign dlab = lcr[7];
+
+
+
