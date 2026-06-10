a50144bc03967d83ba7a509297a6f7e923a47869
Mohammed Aadil <adhilmohamed7@gmail.com>
2026-06-10 12:25:27 +0000

    updated

--- uvm_tb/apb_agent/apb_sequence.sv
+++ uvm_tb/apb_agent/apb_sequence.sv
@@ -1,1 +1,44 @@
-class apb_sequence extends
+class apb_sequence extends uvm_sequence#(apb_xtn);
+    `uvm_object_utils(apb_sequence)
+
+    function new(string name = "apb_sequence");
+        super.new(name);
+    endfunction: new
+
+endclass: apb_sequence
+
+class lcr_rw_seq extends apb_sequence ;
+    `uvm_object_utils(lcr_rw_seq)
+
+    function new(string name = "lcr_rw_seq");
+        super.new(name);
+    endfunction: new
+
+    task body();
+        apb_xtn xtn;
+    //--------------------------------       
+    // Write to LCR
+    //--------------------------------
+    xtn=apb_xtn::type_id::create("xtn");
+
+    start_item(xtn);
+    xtn.addr = 8'h03; // LCR address
+    xtn.data = 8'h1B; // Set DLAB=1 to access DLL and DLM
+    xtn.write = 1; // Write operation
+    finish_item(xtn);
+
+    //--------------------------------
+    // Read from LCR
+    //--------------------------------
+    start_item(xtn);
+    xtn.addr = 8'h03; // LCR address
+    xtn.write = 0; // Read operation
+    finish_item(xtn);
+    endtask: body
+endclass: lcr_rw_seq
+
+
+
+
+
+        
--- uvm_tb/apb_agent/apb_sequencer.sv
+++ uvm_tb/apb_agent/apb_sequencer.sv
@@ -1,1 +1,7 @@
-class apb_sequencer
+class apb_sequencer extends uvm_sequencer#(apb_xtn);
+    `uvm_component_utils(apb_sequencer)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction: new
+endclass: apb_sequencer
