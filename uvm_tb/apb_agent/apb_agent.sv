6e9f793b87ffbca01de32b4a263a925cbe8c85ce
Mohammed Aadil <adhilmohamed7@gmail.com>
2026-06-11 06:09:19 +0000

    updated

---
+++ uvm_tb/apb_agent/apb_agent.sv
@@ -0,0 +1,29 @@
+class apb_agent extents uvm_agent;
+  `uvm_component_utils(apb_agent)
+  
+  apb_driver apb_drv;
+  apb_sequencer apb_seqr;
+  //apb_monitor apb_mon;
+  
+  
+  function new(string name"apb_agent",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    apb_mon=apb_monitor::type_id::create("apb_mon",this);
+    //if(is_active)
+    //begin
+    apb_drv=apb_driver::type_id::create("apb_drv",this);
+    apb_seqr=apb_sequencer::type_id::create("apb_seqr",this);
+    //end
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    apb_drv.seq_item_port.connect(apb_seqr.seq_item_export);
+  endfunction
+  
+  
+endclass
