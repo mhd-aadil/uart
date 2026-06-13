5f4ab33c413647d56b1a4a8ca314503bf68eb9d6
Mohammed Aadil <adhilmohamed7@gmail.com>
2026-06-13 10:31:45 +0000

    updated files

--- uvm_tb/tb/uart_env.sv
+++ uvm_tb/tb/uart_env.sv
@@ -1,6 +1,7 @@
 class uart_env extends uvm_env;
   `uvm_component_utils(uart_env);
   apb_agent apb_agt;
+  uart_agent uart_agt;
   
   function new(string name="uart_env",uvm_component parent);
     super.new(name,parent);
@@ -9,6 +10,7 @@ class uart_env extends uvm_env;
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     apb_agt=apb_agent::type_id::create("apb_agt",this);
+    uart_agt=uart_agent::type_id::create("uart_agt",this);
   endfunction
   
 endclass
--- uvm_tb/uart_agent/uart_monitor .sv
+++ uvm_tb/uart_agent/uart_monitor .sv
@@ -16,4 +16,60 @@ class uart_monitor extends uvm_monitor;
   function void build_phase(uvm_phase phase);
 
      super.build_phase(phase);
-     uvm_config_db#(virtual uart_if)::
+     if(!uvm_config_db#(virtual uart_if)::get(this,"","uart_vif",uart_vif))
+       `uvm_fatal("NO VIF","VIF GETTING FAILLED IN UART MONITOR")
+       
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    uart_xtn uxtn;
+  	forever 
+      begin
+        @(negedge uart_vif.tx);
+        uxtn=uart_xtn::type_id::create("uxtn");
+        
+		 //----------------------------------
+         // Sample in middle of start bit
+         //----------------------------------
+        #(uart_vif.bit_time/2);
+
+        if(uart_vif.tx != 1'b0)
+           continue;
+        
+        #(uart_vif.bit_time);
+        
+        for(int i=0;i<8;i=i++)
+          begin
+            uxtn.data[i]=uart_vif.tx;
+            
+            #(uart_vif.bit_time);
+            
+          end
+        if(uart_vif.tx!=1'b1)
+          uxtn.framing_error = 1'b1;
+        else
+          uxtn.framing_error = 1'b0;
+        ap.write(uxtn);
+         `uvm_info(
+                "UART_MON",
+                $sformatf(
+                    "RX BYTE = %02h",
+                    uxtn.data
+                ),
+                UVM_MEDIUM
+            );
+
+        end
+
+    endtask
+          
+            
+       
+        
+        
+        
+        
+        
+    
+       
+       
